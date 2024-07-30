9945a5eb6addd5663ed32af5434957e0bb76d055
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2024-07-30 11:20:29 -0300

    versão 1.1 Font
    troca da fonte de arial para Montserrat

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     <title>jogo em java</title>
 
     <link rel="stylesheet" href="css/style.css">
+
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
 </head>
+
 <body>
+
+    <!-- Titulo do jogo -->
+
     <header>
-    <h1 class="text-center">Nome do Game</h1>
+        <h1 class="text-center">Nome do Game</h1>
     </header>
+
+    <!-- conteudo prinscipal -->
 
     <main>
         <h2>Hello Word</h2>
     </main>
 
-    <script src="js/bootstrap.bundle.min.js"></script>
-    
+
 </body>
+
+    <!-- codigos js -->
+
+     <script src="js/bootstrap.bundle.min.js"></script>
+
 </html>
