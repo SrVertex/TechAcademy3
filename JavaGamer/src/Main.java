0779d5d328fe347f8c53d1acf125d13f35b1a1d7
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2024-08-24 19:42:17 -0300

    versão 2.4 comand help em java usando Array

--- JavaGamer/src/Main.java
+++ JavaGamer/src/Main.java
@@ -1,5 +1,18 @@
+import Comandos.ComandoPrompt;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+
+        while (true) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite o comando");
+            String help = scanner.nextLine();
+
+            ComandoPrompt ComandoHelp = new ComandoPrompt(help);
+        }
     }
 }
