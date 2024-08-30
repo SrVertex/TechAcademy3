1f30e0c5b5fa95ba5b65f590834c17a60e32d2a4
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2024-08-30 14:18:22 -0300

    integração do java ao fron-end

--- JavaGamer/src/Comandos/ComandoPrompt.java
+++ JavaGamer/src/Comandos/ComandoPrompt.java
@@ -2,6 +2,8 @@ package Comandos;
 
 public class ComandoPrompt {
 
+    private String resposta;
+
     public ComandoPrompt(String help) {
 
         String[] comandos = help.split(" ");
@@ -9,27 +11,21 @@ public class ComandoPrompt {
 
         switch (ComandoDigitado.toUpperCase()) {
             case "HELP":
-
-                System.out.println(
-                        "\n" +
-                            "Help é um comando de mostra comandos usaveis no durante o game.\n" +
-                            "Get: \n" +
-                            "Inventory: O comando de inventario mostrara todos os itens que o jogador adquiriu duarante o progreço do jogo. \n" +
-                            "Reset: Cuidado com esse comando, o reset limpara todo o progreço do jogador!!!!!!!! \n" +
-                            "Save: Esse é um comando para salvar o progreço do game.\n" +
-                        "\n"
-                );
-
+                resposta = "Help é um comando de mostra comandos usaveis no durante o game.\n";
+                resposta += "Get: \n";
+                resposta += "Inventory: O comando de inventario mostrara todos os itens que o jogador adquiriu duarante o progreço do jogo. \n";
+                resposta += "Reset: Cuidado com esse comando, o reset limpara todo o progreço do jogador!!!!!!!! \n";
+                resposta += "Save: Esse é um comando para salvar o progreço do game.\n";
                 break;
             case "GET_MADEIRA":
-                System.out.println("Blocos de maderira coletados");
-
+                resposta = "Blocos de maderira coletados";
                 break;
             default:
-
-                System.out.println("comando invalido Digite novamente!");
-
+                resposta = "comando invalido Digite novamente!";
         }
     }
-}
 
+    public String getResposta() {
+        return resposta;
+    }
+}
--- index.php
+++ index.php
@@ -1,24 +1,6 @@
-    <!--  BACK-END EM PHP  -->
+<!--  BACK-END EM PHP  -->
 
-    <?php
-// session_start();
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     if (isset($_POST['inputText'])) {
-//         if (!isset($_SESSION['historico'])) {
-//             $_SESSION['historico'] = [];
-//         }
-//         $inputText = htmlspecialchars($_POST['inputText']);
-//         $url = "http://localhost:8080/api/comando?comando=" . urlencode($inputText);
-//         $ch = curl_init($url);
-//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//         $response = curl_exec($ch);
-//         curl_close($ch);
-//         $_SESSION['historico'][] = $response;
-//     } elseif (isset($_POST['clear'])) {
-//         $_SESSION['historico'] = [];
-//     }
-// }
+<?php
 
 session_start();
 
@@ -28,136 +10,150 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['historico'] = [];
         }
         $inputText = htmlspecialchars($_POST['inputText']);
-        $_SESSION['historico'][] = $inputText;
+        $url = "http://localhost:8080/api/comando?comando=" . urlencode($inputText);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $_SESSION['historico'][] = $response;
     } elseif (isset($_POST['clear'])) {
         $_SESSION['historico'] = [];
     }
 }
+
+// session_start();
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     if (isset($_POST['inputText'])) {
+//         if (!isset($_SESSION['historico'])) {
+//             $_SESSION['historico'] = [];
+//         }
+//         $inputText = htmlspecialchars($_POST['inputText']);
+//         $_SESSION['historico'][] = $inputText;
+//     } elseif (isset($_POST['clear'])) {
+//         $_SESSION['historico'] = [];
+//     }
+// }
 
 ?>
 
 
 
 
-    <!--  BACK-END EM PHP  -->
+<!--  BACK-END EM PHP  -->
 
-    <!DOCTYPE html>
-    <html lang="pt-br">
+<!DOCTYPE html>
+<html lang="pt-br">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-            rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
-        <title>jogo em java</title>
+    <title>jogo em java</title>
 
-        <link rel="shortcut icon" href="imagens/logo2.jpg">
+    <link rel="shortcut icon" href="imagens/logo2.jpg">
 
-        <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-        <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
-    </head>
+</head>
 
-    <body>
+<body>
 
 
-        <!-- Titulo do jogo -->
-        <header>
+    <!-- Titulo do jogo -->
+    <header>
 
-            <div class="logo">
-                <img src="imagens/logo1.jpg" alt="">
-                <h2>Java</h2>
+        <div class="logo">
+            <img src="imagens/logo1.jpg" alt="">
+            <h2>Java</h2>
+        </div>
+
+
+        <!-- buttons -->
+        <nav>
+            <form action="" method="post">
+                <button id="reset" type="submit" name="clear" style="margin: 0 10px 0 0 ;">Reset</button>
+                <button type="" href="">Save</button>
+            </form>
+        </nav>
+
+    </header>
+
+    <!-- conteudo prinscipal -->
+    <main>
+
+        <section id="main">
+            <!-- conteiner de historia -->
+            <div class="historia">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores debitis excepturi asperiores et,
+                    maiores consequatur ex voluptate, sed ratione consequuntur praesentium aut quisquam sequi saepe
+                    culpa, officia odit? Repellendus, nihil!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores debitis excepturi asperiores et,
+                </p>
             </div>
 
 
-            <!-- buttons -->
-            <nav>
-                <form action="" method="post">
-                    <button id="reset" type="submit" name="clear" style="margin: 0 10px 0 0 ;">Reset</button>
-                    <button type="" href="">Save</button>
-                </form>
-            </nav>
-
-        </header>
-
-        <!-- conteudo prinscipal -->
-        <main>
-
-            <section id="main">
-                <!-- conteiner de historia -->
-                <div class="historia">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores debitis excepturi asperiores et,
-                        maiores consequatur ex voluptate, sed ratione consequuntur praesentium aut quisquam sequi saepe
-                        culpa, officia odit? Repellendus, nihil!
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores debitis excepturi asperiores et,
-                    </p>
-                </div>
-
-
-                <div class="form-bg">
-                    <div class="form-center">
-
-                        <?php
-                        if (isset($_SESSION['history'])) {
-
-                            foreach ($_SESSION['historico'] as $item) {
-                                echo "<p>$item</p>";
-                            }
-
+            <div class="form-bg">
+                <div class="form-center">
+                    <?php
+                    if (isset($_SESSION['historico'])) {
+                        foreach ($_SESSION['historico'] as $item) {
+                            echo "<p>$item</p>";
                         }
-                        ?>
-
-
-                    </div>
+                    }
+                    ?>
                 </div>
+            </div>
 
 
-                <!-- barra de pesquisa  -->
-        <div class="form-psq">
+            <!-- barra de pesquisa  -->
+            <div class="form-psq">
                 <form method="POST" action="">
                     <!-- input de pesquisa -->
                     <input type="text" id="inputText" name="inputText" placeholder="Digite os codigos" required>
 
                     <!-- botão de pesquisa -->
                     <button type="submit">
-                    <i class="bi bi-arrow-up-square-fill"></i>
+                        <i class="bi bi-arrow-up-square-fill"></i>
                     </button>
 
                 </form>
 
-                </div>
-            </section>
+            </div>
+        </section>
 
 
-    </body>
+</body>
 
-    <!-- COMANDO EM JAVSSCRIPT -->
-    <!-- COMANDO EM JAVSSCRIPT -->
-    <!-- COMANDO EM JAVSSCRIPT -->
+<!-- COMANDO EM JAVSSCRIPT -->
+<!-- COMANDO EM JAVSSCRIPT -->
+<!-- COMANDO EM JAVSSCRIPT -->
 
-    <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-    <script>
-        AOS.init();
-    </script>
-    <script>
-        const btn = document.querySelector('#reset')
+<script>
+    AOS.init();
+</script>
+<script>
+    const btn = document.querySelector('#reset')
 
-        btn.addEventListener('click', () => {
-            location.reload()
-        })
+    btn.addEventListener('click', () => {
+        location.reload()
+    })
 
-    </script>
+</script>
 
-    </html>
+</html>
