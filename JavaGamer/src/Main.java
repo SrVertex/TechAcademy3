d447e15fdc5e9cd2ae44a71cec5aa4f4c1ed9533
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2024-09-07 11:03:01 -0300

    teste de Gson  itens

--- JavaGamer/src/Main.java
+++ JavaGamer/src/Main.java
@@ -1,9 +1,8 @@
-import Comandos.CenasDAO;
 import Comandos.ItemDAO;
 import com.google.gson.Gson;
+
+import Comandos.CenasDAO;
 import spark.Spark;
-
-
 
 public class Main {
 
@@ -12,21 +11,19 @@ public class Main {
     public static void main(String[] args) {
         try {
 
-//            String saveJson = Gson.toJson(save);
-//            Spark.get("/", (req,res) -> saveJson);
+            // String saveJson = Gson.toJson(save);
+            // Spark.get("/", (req,res) -> saveJson);
 
 
-            
+            Spark.get("cenas/:id", (request, response) -> {
+                Integer idCena = Integer.parseInt(request.params(":id"));
+                return Gson.toJson(CenasDAO.findCenaById(idCena));
+            });
 
-//            Spark.get("cenas/:id", (request, response) -> {
-//                Integer idCena = Integer.parseInt(request.params(":id")) ;
-//                return  Gson.toJson(CenasDAO.findCenaById(idCena));
-//            } );
-
-            Spark.get("item/:id", (request, response) -> {
-                Integer idItem = Integer.parseInt(request.params(":id")) ;
-                return  Gson.toJson(ItemDAO.findItensByScene(idItem));
-            } );
+             Spark.get("item/:id", (request, response) -> {
+            Integer idItem = Integer.parseInt(request.params(":id")) ;
+             return Gson.toJson(ItemDAO.findItensByScene(idItem));
+             } );
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -35,10 +32,10 @@ public class Main {
     }
 }
 
-//port(8080); // Porta em que o servidor Java vai rodar
+// port(8080); // Porta em que o servidor Java vai rodar
 //
-//get("/api/comando", (req, res) -> {
-//String comando = req.queryParams("comando");
-//ComandoPrompt comandoPrompt = new ComandoPrompt(comando);
-//            return comandoPrompt.getResposta();
-//        });
+// get("/api/comando", (req, res) -> {
+// String comando = req.queryParams("comando");
+// ComandoPrompt comandoPrompt = new ComandoPrompt(comando);
+// return comandoPrompt.getResposta();
+// });
