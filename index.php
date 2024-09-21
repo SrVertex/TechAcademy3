0dbbcd75eb4c0eae34a8b8ff28b67eb2e67bfa2a
SrVertex_Notebook <srvertex@outlook.com>
2024-09-21 17:13:31 -0300

    Desistencia Front

--- index.php
+++ index.php
@@ -93,21 +93,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save'])) {
 
 
 
-// //session_start();
+//session_start();
 
-// if ($_GET['comando'] && $_GET['save']) {
-//     $comando = rawurlencode($_GET['comando']);
-//     $save = rawurlencode($_GET['save']);
-//     $conteudo = file_get_contents("http://localhost:4567/{$comando}/{$save}");
-// } else if(isset($_GET['comando'])) {
-//     $comando = rawurlencode($_GET['comando']);
-//     $conteudo = file_get_contents("http://localhost:4567/{$comando}");
-// } else {
-//     $conteudo = file_get_contents("http://localhost:4567");
-// }
+if ($_GET['comando'] && $_GET['save']) {
+    $comando = rawurlencode($_GET['comando']);
+    $save = rawurlencode($_GET['save']);
+    $conteudo = file_get_contents("http://localhost:4567/{$comando}/{$save}");
+} else if(isset($_GET['comando'])) {
+    $comando = rawurlencode($_GET['comando']);
+    $conteudo = file_get_contents("http://localhost:4567/{$comando}");
+} else {
+    $conteudo = file_get_contents("http://localhost:4567");
+}
 
 
-// $arrayAssociativo = json_decode($conteudo);
+$arrayAssociativo = json_decode($conteudo);
 
 
 
