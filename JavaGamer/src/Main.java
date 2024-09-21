bc5a4e5dc798650eba2b4f66b2b34f230132a981
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2024-09-21 17:25:09 -0300

    continualção

--- JavaGamer/src/Main.java
+++ JavaGamer/src/Main.java
@@ -1,10 +1,18 @@
 
+import Comandos.CenasDAO;
+import Comandos.Console;
 import Controle.AntesDoJogoController;
 import Controle.DuranteOJogoController;
 
+import Model.Cenas;
+import Repositorio.ComandoService;
 import com.google.gson.Gson;
 
 import spark.Spark;
+
+import java.util.Scanner;
+
+import static spark.Spark.post;
 
 
 public class Main {
@@ -13,36 +21,38 @@ private static final Gson Gson = new Gson();
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
 
-        //Rota 1 http://localhost:4567/{o comando vai aqui}
-        Spark.get("/:comando", new AntesDoJogoController(Gson));
+        System.out.println("Bem-vindo ao Jogo! Digite 'start' para iniciar ou 'help' para ver os comandos disponíveis.");
 
-        //Rota 2 http://localhost:4567/{comando}/{save}
-        Spark.get("/:comando/:save", new DuranteOJogoController(Gson));
+        while (true) {
+            System.out.print("> ");
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Saindo do jogo. Até a próxima!");
+                break;
+            }
+
+            // Processar o comando com o ComandoService
+            ComandoService comandoService = new ComandoService(input);
+            Console resultado = comandoService.getResultadoConsole();
+
+            // Exibir a mensagem de retorno no console
+            System.out.println(resultado.getMensagem());
+        }
+
+        scanner.close();
 
 
+//        //Rota 1 http://localhost:4567/{o comando vai aqui}
+//        Spark.get("/:comando", new AntesDoJogoController(Gson));
 //
-//        try {
-//           Spark.get("cenas/:id", (request, response) -> {
-//                Integer idCena = Integer.parseInt(request.params(":id"));
-//               Integer idInventory = Integer.parseInt(request.params(":id"));
-//                   // Obter dados de CenasDAO
-//              String cenasJson = Gson.toJson(CenasDAO.findCenaById(idCena));
-//
-//               // Obter dados de InventarioDAO
-//               String inventarioJson = Gson.toJson(InventarioDAO.findInvetarioById(idInventory));
-//
-//              // Combinar os resultados em um único JSON
-//               JsonObject combinedResult = new JsonObject();
-//               combinedResult.add("cenas", new JsonParser().parse(cenasJson));
-//                combinedResult.add("inventario", new JsonParser().parse(inventarioJson));
-//
-//               return combinedResult.toString();
-//            });
-//
-//
-//       } catch (Exception e) {
-//           throw new RuntimeException(e);
-//       }
+//        //Rota 2 http://localhost:4567/{comando}/{save}
+//        Spark.get("/:comando/:save", new DuranteOJogoController(Gson));
+
+
+
     }
 }
