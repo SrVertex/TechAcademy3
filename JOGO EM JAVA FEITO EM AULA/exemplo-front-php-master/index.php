9241010f5a7284482947f4efc9690b6eb33fed3f
SrVertex_Notebook <srvertex@outlook.com>
2024-09-21 15:31:39 -0300

    Teste Api1

--- JOGO EM JAVA FEITO EM AULA/exemplo-front-php-master/index.php
+++ JOGO EM JAVA FEITO EM AULA/exemplo-front-php-master/index.php
@@ -5,7 +5,7 @@
 if ($_GET['comando'] && $_GET['save']) {
     $comando = rawurlencode($_GET['comando']);
     $save = rawurlencode($_GET['save']);
-    $conteudo = file_get_contents("http://localhost:4567/{$comando}/{$save}");
+    $conteudo = file_get_contents("http://localhost:4567/{$comando}");
 } else if(isset($_GET['comando'])) {
     $comando = rawurlencode($_GET['comando']);
     $conteudo = file_get_contents("http://localhost:4567/{$comando}");
@@ -17,7 +17,12 @@ if ($_GET['comando'] && $_GET['save']) {
 $arrayAssociativo = json_decode($conteudo);
 
 
-//$_SESSION['historico'] = isset($_SESSION['historico']) ? array_merge($_SESSION['historico'], $arrayAssociativo->messages) : [];
+$_SESSION['historico'] = isset($_SESSION['historico']) ? array_merge($_SESSION['historico'], $arrayAssociativo->messages) : [];
 
 
 include "template.phtml";
+
+
+
+
+
--- index.php
+++ index.php
@@ -85,4 +85,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save'])) {
 
 
 
+
+
+
+
+
+
+
+
+// //session_start();
+
+// if ($_GET['comando'] && $_GET['save']) {
+//     $comando = rawurlencode($_GET['comando']);
+//     $save = rawurlencode($_GET['save']);
+//     $conteudo = file_get_contents("http://localhost:4567/{$comando}/{$save}");
+// } else if(isset($_GET['comando'])) {
+//     $comando = rawurlencode($_GET['comando']);
+//     $conteudo = file_get_contents("http://localhost:4567/{$comando}");
+// } else {
+//     $conteudo = file_get_contents("http://localhost:4567");
+// }
+
+
+// $arrayAssociativo = json_decode($conteudo);
+
+
+
+
+
+
+
+
+// session_start();
+
+// if ($_GET['comando'] && $_GET['save']) {
+//     $comando = rawurlencode($_GET['comando']);
+//     $save = rawurlencode($_GET['save']);
+//     echo "Rota 1";
+//     $conteudo = file_get_contents("http://localhost:4567/{$comando}/{$save}");
+// } else if(isset($_GET['comando'])) {
+//     $comando = rawurlencode($_GET['comando']);
+//     echo $comando;
+//     $conteudo = file_get_contents("http://localhost:4567/{$comando}");
+// } else {
+//     echo "Rota 3";
+//     $conteudo = file_get_contents("http://localhost:4567");
+// }
+
+
+// $arrayAssociativo = json_decode($conteudo);
+
+// print_r($arrayAssociativo);
+// //$_SESSION['historico'] = isset($_SESSION['historico']) ? array_merge($_SESSION['historico'], $arrayAssociativo->messages) : [];
+
+// $messages = $arrayAssociativo->mensagem;
+
+
+
+
+
 include "index.phtml";
