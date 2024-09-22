d768caffa1154ae95f6b3bfd6dc882cb97c44532
SrVertex_Notebook <srvertex@outlook.com>
2024-09-22 19:07:48 -0300

    FUNCIONOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- JavaGamer/src/Comandos/CenasDAO.java
+++ JavaGamer/src/Comandos/CenasDAO.java
@@ -8,67 +8,84 @@ import java.util.List;
 
 public class CenasDAO {
 
-
-        public static Integer proximaCenas = 1;
-
-
+    public static Integer proximaCenas = 1;
 
     public static Cenas findCenaById(Integer id) throws SQLException {
-
-                    // CONECXAO A TABLA CENAS
-        Connection conn = Mysql.getConnection();
-        String sql = "SELECT * FROM cena WHERE id_cena = ?";
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setInt(proximaCenas, id);
-        ResultSet rs = stmt.executeQuery();
-
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
         Cenas cenas = new Cenas();
 
-        if (rs.next()) {
+        try {
+            conn = Mysql.getConnection();
+            String sql = "SELECT * FROM cena WHERE id_cena = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
 
-                    // ITENS DO BANCO DE DADOS
-            cenas.setId_cena(rs.getInt("id_cena"));
-            cenas.setDescricao_cena(rs.getString("descricao_cena"));
-            cenas.setNome_cena(rs.getString("nome_cena"));
-            cenas.setItem_cenario(rs.getString("item_cenario"));
-            cenas.setTextoPositivo_cena(rs.getString("textoPositivo_cena"));
-            cenas.setTextoNegativo_cena(rs.getString("textoNegativo_cena"));
-            cenas.setItens1(ItemDAO.findItensByScene(cenas));
-
+            if (rs.next()) {
+                cenas.setId_cena(rs.getInt("id_cena"));
+                cenas.setDescricao_cena(rs.getString("descricao_cena"));
+                cenas.setNome_cena(rs.getString("nome_cena"));
+                cenas.setItem_cenario(rs.getString("item_cenario"));
+                cenas.setTextoPositivo_cena(rs.getString("textoPositivo_cena"));
+                cenas.setTextoNegativo_cena(rs.getString("textoNegativo_cena"));
+                cenas.setItens1(ItemDAO.findItensByScene(cenas));
+            }
+        } finally {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
         }
 
         return cenas;
-
     }
+
     public static void insertCena(Cenas cenas) throws SQLException {
-        Connection connection = Mysql.getConnection();
-        String insert = "INSERT INTO cena (descricao_cena) VALUES (?);";
-        PreparedStatement ps = connection.prepareStatement(insert);
-        ps.setString(CenasDAO.proximaCenas, cenas.getDescricao_cena());
-        ps.execute();
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = Mysql.getConnection();
+            String insert = "INSERT INTO cena (descricao_cena) VALUES (?);";
+            ps = connection.prepareStatement(insert);
+            ps.setString(1, cenas.getDescricao_cena());
+            ps.execute();
+        } finally {
+            if (ps != null) ps.close();
+            if (connection != null) connection.close();
+        }
     }
 
-                     // LISTA DE ITENS DA CENAS
     public static List<Cenas> findAll() throws SQLException {
-        Connection connection = Mysql.getConnection();
-        String sql = "select * from cena;";
-        PreparedStatement ps = connection.prepareStatement(sql);
-        ResultSet resultSet = ps.executeQuery();
-
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
         List<Cenas> cenas = new ArrayList<>();
-        while (resultSet.next()) {
 
-            Cenas cena = new Cenas();
+        try {
+            connection = Mysql.getConnection();
+            String sql = "SELECT * FROM cena;";
+            ps = connection.prepareStatement(sql);
+            resultSet = ps.executeQuery();
 
-            cena.setId_cena(resultSet.getInt("id_cena"));
-            cena.setDescricao_cena(resultSet.getString("descricao_cena"));
-            cena.setNome_cena(resultSet.getString("nome_cena"));
-            cena.setItem_cenario(resultSet.getString("item_cenario"));
-            cena.setTextoPositivo_cena(resultSet.getString("textoPositivo_cena"));
-            cena.setTextoNegativo_cena(resultSet.getString("textoNegativo_cena"));
+            while (resultSet.next()) {
+                Cenas cena = new Cenas();
+                cena.setId_cena(resultSet.getInt("id_cena"));
+                cena.setDescricao_cena(resultSet.getString("descricao_cena"));
+                cena.setNome_cena(resultSet.getString("nome_cena"));
+                cena.setItem_cenario(resultSet.getString("item_cenario"));
+                cena.setTextoPositivo_cena(resultSet.getString("textoPositivo_cena"));
+                cena.setTextoNegativo_cena(resultSet.getString("textoNegativo_cena"));
 
-            cenas.add(cena);
+                cenas.add(cena);
+            }
+        } finally {
+            if (resultSet != null) resultSet.close();
+            if (ps != null) ps.close();
+            if (connection != null) connection.close();
         }
+
         return cenas;
     }
 
